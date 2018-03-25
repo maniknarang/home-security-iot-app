33d48f795919f5f257e79a6327021d0aed62796d
Kaustubh Lall <klall@ucsd.edu>
2018-03-25 01:50:08 -0700

    server almost up

--- server/hackiot/camera/views.py
+++ server/hackiot/camera/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import os
 def index(request):
-    return(HttpResponse("MEOW burtis"))
+	# check which windows are open
+	os.system('cd ..')
+	win1 = os.path.isdir('win1')
+	win2 = os.path.isdir('win2')
+	print("Checking win1 : %s, win2: %s" % (win1, win2))
+	d = {'win1' : win1, 'win2' : win2}
+	return(render(request, 'list.html'))
 
 # Create your views here.
