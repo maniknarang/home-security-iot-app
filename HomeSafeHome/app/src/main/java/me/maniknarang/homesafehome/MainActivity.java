79ea1c48d0f0189a6ff93d7b389fa15b59f88816
Manik Narang <narang_manik@rocketmail.com>
2018-03-25 01:43:34 -0700

    IoT Android app - remote access to security camera

---
+++ HomeSafeHome/app/src/main/java/me/maniknarang/homesafehome/MainActivity.java
@@ -0,0 +1,34 @@
+package me.maniknarang.homesafehome;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webview.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.loadUrl("http://10.120.3.154:8090/camera.mjpeg");
+    }
+}
