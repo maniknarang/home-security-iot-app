4e9e93070abeaf1a834b26680d3753d443520430
root <root@DESKTOP-05BG6C4.localdomain>
2018-04-05 12:35:46 -0700

    lol

--- server/hackiot/node/views.py
+++ server/hackiot/node/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return(render(request, 'list.html'))
-
 # Create your views here.
+def index(request): return HttpResponse("done")
